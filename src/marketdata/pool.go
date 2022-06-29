0e60dc706de6af2759eefb82a9a676eebcf63695
Jakson Kallio <jaksonkallio@gmail.com>
2022-06-29 08:19:07 -0500

    Re-design profit calculation method

--- src/marketdata/pool.go
+++ src/marketdata/pool.go
@@ -24,13 +24,10 @@ type Pool struct {
 
 // Pool stats over a specified date range.
 type PoolDayRangeStats struct {
-	StartDate           time.Time
-	EndDate             time.Time
-	SumTotalValueLocked float64
-	SumFees             float64
-	ProfitOverRange     float64
-	ProfitAnnualized    float64
-	Length              int
+	StartDate        time.Time
+	EndDate          time.Time
+	ProfitOverRange  float64
+	ProfitAnnualized float64
 }
 
 // Iteratively fetches all pools with fee data in date range.
@@ -50,8 +47,10 @@ func FetchAllPools(dateRangeStart time.Time, dateRangeEnd time.Time, minTvl floa
 				FeesUsd graphql.String `graphql:"feesUSD"`
 				TvlUsd  graphql.String `graphql:"tvlUSD"`
 				Date    graphql.Int    `graphql:"date"`
+				// Assumes that we want to fetch dates in the start date and end date inclusively.
 			} `graphql:"poolDayData(where: { date_gte: $dateRangeStart, date_lte: $dateRangeEnd })"`
-		} `graphql:"pools(first: $count, where: { id_gt: $lastId } )"`
+			// Paginate using the best-practice outlined in The Graph documentation instead of using `skip`.
+		} `graphql:"pools(first: $count, where: { id_gt: $lastId })"`
 	}
 
 	page := 0
@@ -75,12 +74,12 @@ func FetchAllPools(dateRangeStart time.Time, dateRangeEnd time.Time, minTvl floa
 		for _, resPool := range res.Pools {
 			// Init a range stats object.
 			poolDayRangeStats := PoolDayRangeStats{
-				Length:    len(resPool.PoolDays),
 				StartDate: dateRangeStart,
 				EndDate:   dateRangeEnd,
 			}
 
 			exceedsMinTvl := false
+			var profit float64
 
 			// Iterate over each pool day.
 			for _, resPoolDay := range resPool.PoolDays {
@@ -101,21 +100,26 @@ func FetchAllPools(dateRangeStart time.Time, dateRangeEnd time.Time, minTvl floa
 					exceedsMinTvl = true
 				}
 
-				// Add to our sums.
-				poolDayRangeStats.SumFees += feesUsd
-				poolDayRangeStats.SumTotalValueLocked += tvlUsd
+				// Skip where TVL is non-positive, i.e. we'd get a divide-by-zero issue when we calculate profit.
+				if tvlUsd <= 0 {
+					continue
+				}
+
+				// The profit for this day is calculated as the fees collected divided by the total locked value.
+				// This gives us the profit per $1 USD locked ("deposited") on this day.
+				// We're summing all of the daily profit-per-$1 numbers into a `profit` float.
+				profit += feesUsd / tvlUsd
 			}
 
 			// Verify that on some date in the range, the min TVL was exceeded.
 			// Ignore pools where the min TVL was never exceeded in the provided date range.
 			if exceedsMinTvl {
-				if poolDayRangeStats.SumTotalValueLocked != 0 {
-					poolDayRangeStats.ProfitOverRange = poolDayRangeStats.SumFees / poolDayRangeStats.SumTotalValueLocked
+				poolDayRangeStats.ProfitOverRange = profit
 
-					if poolDayRangeStats.Length > 0 {
-						// Annualized profit assumes there are exactly 365 days in a year.
-						poolDayRangeStats.ProfitAnnualized = (poolDayRangeStats.ProfitOverRange / float64(poolDayRangeStats.Length)) * 365
-					}
+				if poolDayRangeStats.Length() > 0 {
+					// Annualized profit assumes there are exactly 365 days in a year.
+					// Divide profit received over the range by the number of days in the range to get the average daily return, then multiply by 365 to get the annual return.
+					poolDayRangeStats.ProfitAnnualized = (poolDayRangeStats.ProfitOverRange / poolDayRangeStats.Length()) * 365
 				}
 
 				// Add new pool to the resulting pools slice.
@@ -163,12 +167,17 @@ func MostProfitablePool(pools []*Pool) *Pool {
 // Pretty-prints the pool by returning a string.
 func (pool *Pool) Pretty() string {
 	return fmt.Sprintf(
-		"\n\tPool Address: %s\n\tTokens: %s <-> %s\n\tRange Length: %d\n\tProfit Over Range (Earned per $1 USD Deposited): %f\n\tProfit Annualized (APR): %.2f%%",
+		"\n\tPool Address: %s\n\tTokens: %s <-> %s\n\tRange Length: %f\n\tProfit Over Range (Earned per $1 USD Deposited): %f\n\tProfit Annualized (APR): %.2f%%",
 		pool.Id,
 		pool.Token0Name,
 		pool.Token1Name,
-		pool.DayRangeStats.Length,
+		pool.DayRangeStats.Length(),
 		pool.DayRangeStats.ProfitOverRange,
 		math.Round(pool.DayRangeStats.ProfitAnnualized*10000)/float64(100),
 	)
+}
+
+// Returns the length of the pool stats range in days.
+func (poolDayRangeStats *PoolDayRangeStats) Length() float64 {
+	return poolDayRangeStats.EndDate.Sub(poolDayRangeStats.StartDate).Hours() / 24
 }
